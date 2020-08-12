b95683eaf985727ea20a5c596c6269357524d518
AbdElrahman Ibrahim Zaki <62768502+abdelrahman179@users.noreply.github.com>
2020-08-12 02:09:08 +0200

    Create STD_TYPES.h

---
+++ STD_TYPES.h
@@ -0,0 +1,27 @@
+/****************************************************************/
+/*   Author             :    AbdElrahman Ibrahim Zaki		    */
+/*	 Date 				:                                         */
+/*	 Version 			:    1.0V 							 	*/
+/*	 Description 		:    Standart C Types                       */
+/****************************************************************/
+
+// Guard File
+
+#ifndef STD_STYPES_H
+#define STD_STYPES_H
+
+typedef unsigned char		 u8;
+typedef unsigned short int	 u16;
+typedef unsigned int		 u32;
+
+typedef signed char		 	 s8;
+typedef signed short int	 s16;
+typedef signed int		 	 s32;
+
+typedef float 				 f32;
+typedef double 				 f64;
+
+
+
+
+#endif
